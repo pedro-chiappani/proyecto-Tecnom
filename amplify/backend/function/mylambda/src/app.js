6436406c629f98a36abbc1c597b04ab72907ddda
Pedro Chiappani <pepeelchapa@gmail.com>
2021-05-22 19:21:01 -0300

    acces control

--- amplify/backend/function/mylambda/src/app.js
+++ amplify/backend/function/mylambda/src/app.js
@@ -14,8 +14,12 @@ app.use(awsServerlessExpressMiddleware.eventContext())
  
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "")
-  res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+  );
   next()
 });
  
